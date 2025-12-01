c6211c78a5088acc8596c89f0e84ea7c680bcc9c
utkarsh-291 <utkarshgupte2912@gmail.com>
2025-12-01 20:53:02 +0530

    Update api for cloud deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:8000/api/' });
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api/';
+
+const api = axios.create({ baseURL });
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('access_token');
